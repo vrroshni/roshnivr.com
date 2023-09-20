efd48ce55f9c17474a136d78af0d549ff64822b7
Roshni V R <vrroshni2001@gmail.com>
2023-09-20 15:22:50 +0530

    meta data

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,12 +3,13 @@ import type { Metadata } from 'next'
 import Navbar from './components/navbar/Navbar'
 import LeftSide from './components/sides/LeftSide'
 import RisghtSide from './components/sides/RisghtSide'
+import Head from 'next/head'
 
 
-export const metadata: Metadata = {
-  title: 'Roshni V R',
-  description: 'Python full stack Developer',
-}
+// export const metadata: Metadata = {
+//   title: 'Roshni V R',
+//   description: 'Python full stack Developer',
+// }
 
 export default function RootLayout({
   children,
@@ -17,6 +18,16 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className='scroll-smooth'>
+       <Head>
+        <title>
+          iPhone 12 XS Max For Sale in Colorado - Big Discounts | Apple
+        </title>
+        <meta
+          name="description"
+          content="Check out iPhone 12 XR Pro and iPhone 12 Pro Max. Visit your local store and for expert advice."
+          key="desc"
+        />
+      </Head>
 
       <body className='w-full h-screen font-bodyFont bg-bodyColor text-textLight  overflow-x-hidden overflow-y-scroll' >
         <Navbar />
